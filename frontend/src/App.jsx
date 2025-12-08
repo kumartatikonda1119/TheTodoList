d2b0dc31e05961d697edeeaeb6979da087f17f87
Kumar Tatikonda <kumartatikonda1119@gmail.com>
2025-12-08 23:09:47 +0530

    latest commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
 import Signup from "./components/Signup.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound.jsx";
 import { Toaster } from "react-hot-toast";
-import { Navigate } from "react-router-dom";
+
 function App() {
-  const token = localStorage.getItem("jwt");
-  console.log(token);
+  const [token, setToken] = useState(() => localStorage.getItem("jwt"));
+  console.log("token in App:", token);
 
   return (
     <>
@@ -17,9 +17,9 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={token ? <Home /> : <Navigate to={"/login"} />}
+            element={token ? <Home /> : <Navigate to="/login" />}
           />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setToken={setToken} />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
