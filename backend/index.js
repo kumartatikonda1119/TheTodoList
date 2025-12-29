c30be513b40544fe39007b61dd9575b0aa02ae69
Kumar Tatikonda <kumartatikonda1119@gmail.com>
2025-12-29 23:13:38 +0530

    Remove duplicate path and fileURLToPath imports

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ import userRoute from "../backend/routes/route.user.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
