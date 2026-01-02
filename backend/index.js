2a2f74ffbfe6e624efb3df64177ae90c27467d9e
Kumar Tatikonda <kumartatikonda1119@gmail.com>
2026-01-02 11:02:51 +0530

    changed url of todolist

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,9 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-// Load environment variables as early as possible
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -30,13 +29,11 @@ app.use("/todo", todoRoute);
 
 app.use("/user", userRoute);
 
-// Serve static files from the React app build directory
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Catch all handler: send back React's index.html file for any non-API routes
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port  http://localhost:${PORT}`);
